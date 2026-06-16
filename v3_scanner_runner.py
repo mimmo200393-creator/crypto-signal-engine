627d9d64e39947fece3fa2145773617219992020
mimmo200393-creator <mimmo200393@gmail.com>
2026-06-17 00:45:42 +0200

    Update v3_scanner_runner.py

--- v3_scanner_runner.py
+++ v3_scanner_runner.py
@@ -6,8 +6,14 @@ l'Institutional Scanner Framework V3.2 (PAXG_USDT + BTC_USDT).
 Isolato da main.py esistente, stesso pattern di daily_brief_runner.py.
 """
 import os
+import logging
 import yaml
 from core import v3_runner
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+)
 
 config = yaml.safe_load(open("config.yaml"))
 config["TELEGRAM_BOT_TOKEN"] = os.environ.get("TELEGRAM_BOT_TOKEN", "")
